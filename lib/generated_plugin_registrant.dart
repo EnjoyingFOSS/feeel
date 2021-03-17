7cc79221dc346f3c88b591afda55cb7d391b6d98
12people <contact.mirek@pm.me>
2021-03-17 19:02:59 +0100

    Translation fix

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,6 +6,7 @@
 
 import 'package:audioplayers/audioplayers_web.dart';
 import 'package:flutter_tts/flutter_tts_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:wakelock_web/wakelock_web.dart';
@@ -16,6 +17,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   AudioplayersPlugin.registerWith(registrar);
   FlutterTtsPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   WakelockWeb.registerWith(registrar);
