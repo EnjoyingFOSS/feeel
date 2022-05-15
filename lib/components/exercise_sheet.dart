39d0a5c95a8f7924c11828adb38648940d6b95c2
Miroslav Mazel <contact.mirek@protonmail.com>
2022-05-15 12:44:19 +0200

    Exercise sheet redesign

--- lib/components/exercise_sheet.dart
+++ lib/components/exercise_sheet.dart
@@ -27,6 +27,7 @@ import 'package:feeel/screens/workout_detail/components/body_exercise_content.da
 import 'package:feeel/screens/workout_detail/components/head_exercise_content.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../db/asset_helper.dart';
 import '../enums/exercise_type.dart';
@@ -76,16 +77,11 @@ class ExerciseSheet extends StatelessWidget {
             : colorSwatch.getColor(FeeelShade.DARK);
         final licenseColor = fgColor.withAlpha(192);
 
-        final padding = EdgeInsets.fromLTRB(
-            MediaQuery.of(context).padding.left + 16,
-            16,
-            MediaQuery.of(context).padding.right + 16,
-            16);
-
         final markdownLicenseStyle =
             MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
                 p: TextStyle(fontSize: 12, color: licenseColor),
-                a: TextStyle(color: licenseColor));
+                a: TextStyle(
+                    color: licenseColor, decoration: TextDecoration.underline));
 
         final hasDescriptionLicense = exercise.descriptionLicense != null;
         final hasImageLicense = exercise.imageLicense != null;
@@ -137,70 +133,97 @@ class ExerciseSheet extends StatelessWidget {
                                         onBreak: false,
                                         squareRatio: true,
                                         illustration: IllustrationWidget(
-                                            imageAssetString:
-                                                AssetHelper.getImage(imageSlug),
-                                            flipped: exercise.flipped)))))),
+                                          imageAssetString:
+                                              AssetHelper.getImage(imageSlug),
+                                          flipped: exercise.flipped,
+                                        )))))),
               Container(
-                padding: padding,
-                color: bgColor,
-                width: double.infinity,
-                child: Text(
-                  exercise.name.i18n,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.w900,
-                      color: fgColor),
-                ),
-              ),
-              _RowContainer(
-                padding: padding,
-                child: Text(
-                  exercise.description.i18n,
-                  style: TextStyle(color: fgColor),
-                ),
-                bgColor: bgColor,
-              ),
-              if (hasDescriptionLicense ||
-                  hasImageLicense) //todo hide licenses under an expandable button
-                _RowContainer(
-                    padding: padding.copyWith(top: 0),
-                    bgColor: bgColor,
-                    child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(color: fgColor.withAlpha(48), height: 1),
+                  padding: EdgeInsets.fromLTRB(
+                      MediaQuery.of(context).padding.left + 16,
+                      0,
+                      MediaQuery.of(context).padding.right + 16,
+                      16),
+                  color: bgColor,
+                  width: double.infinity,
+                  child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                            child: Text(
+                          exercise.name.i18n,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.w900,
+                              color: fgColor),
+                        )),
+                        Container(
+                          height: 8,
+                        ),
+                        Text(
+                          exercise.description.i18n,
+                          style: TextStyle(color: fgColor),
+                        ),
+                        Container(
+                          height: 16,
+                        ),
+                        if (hasDescriptionLicense || hasImageLicense)
+                          Container(
+                              color: fgColor.withAlpha(48),
+                              height:
+                                  1), //todo hide licenses under an expandable button
+                        Container(
+                          height: 16,
+                        ),
+                        Text(
+                          "Disclaimer".i18n.toUpperCase(),
+                          style: Theme.of(context)
+                              .textTheme
+                              .overline
+                              ?.copyWith(color: licenseColor),
+                        ),
+                        MarkdownBody(
+                            data:
+                                "Exercises and workouts are volunteer-contributed. Neither Feeel nor any volunteer is responsible for the correctness of any info in this app or for your health. Use at your own discretion."
+                                    .i18n,
+                            styleSheet: markdownLicenseStyle,
+                            onTapLink: (text, url, title) =>
+                                _onTapLink(context, text, url, title)),
+                        Container(
+                          height: 16,
+                        ),
+                        if (hasDescriptionLicense)
+                          Text(
+                            "English description license".i18n.toUpperCase(),
+                            style: Theme.of(context)
+                                .textTheme
+                                .overline
+                                ?.copyWith(color: licenseColor),
+                          ),
+                        if (hasDescriptionLicense)
+                          MarkdownBody(
+                              data: exercise.descriptionLicense ?? "",
+                              styleSheet: markdownLicenseStyle,
+                              onTapLink: (text, url, title) =>
+                                  _onTapLink(context, text, url, title)),
+                        if (hasDescriptionLicense)
                           Container(
                             height: 16,
                           ),
-                          if (hasDescriptionLicense)
-                            Text(
-                              "English description license".i18n.toUpperCase(),
+                        if (hasImageLicense)
+                          Text("Image license".i18n.toUpperCase(),
                               style: Theme.of(context)
                                   .textTheme
                                   .overline
-                                  ?.copyWith(color: licenseColor),
-                            ),
-                          if (hasDescriptionLicense)
-                            MarkdownBody(
-                                data: exercise.descriptionLicense ?? "",
-                                styleSheet: markdownLicenseStyle),
-                          if (hasDescriptionLicense)
-                            Container(
-                              height: 16,
-                            ),
-                          if (hasImageLicense)
-                            Text("Image license".i18n.toUpperCase(),
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .overline
-                                    ?.copyWith(color: licenseColor)),
-                          if (hasImageLicense)
-                            MarkdownBody(
-                                data: exercise.imageLicense ?? "",
-                                styleSheet: markdownLicenseStyle)
-                        ]))
+                                  ?.copyWith(color: licenseColor)),
+                        if (hasImageLicense)
+                          MarkdownBody(
+                              data: exercise.imageLicense ?? "",
+                              styleSheet: markdownLicenseStyle,
+                              onTapLink: (text, url, title) =>
+                                  _onTapLink(context, text, url, title))
+                      ]))
             ])),
             SliverFillRemaining(
               hasScrollBody: false,
@@ -211,27 +234,13 @@ class ExerciseSheet extends StatelessWidget {
       },
     );
   }
-}
 
-class _RowContainer extends StatelessWidget {
-  final Widget child;
-  final EdgeInsets padding;
-  final Color bgColor;
-
-  const _RowContainer(
-      {Key? key,
-      required this.child,
-      required this.padding,
-      required this.bgColor})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: padding,
-      color: bgColor,
-      width: double.infinity,
-      child: child,
-    );
+  void _onTapLink(
+      BuildContext context, String text, String? url, String title) async {
+    if (url != null && await canLaunch(url))
+      launch(url);
+    else
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Could not open URL.".i18n)));
   }
 }
