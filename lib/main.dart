91f3c178319ea645df7e6394052f09c045f9338d
Miroslav Mazel <contact.mirek@protonmail.com>
2022-06-16 11:47:40 +0200

    Updating packages

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ import 'package:i18n_extension/i18n_widget.dart';
 import 'db/notification_helper.dart';
 import 'i18n/locale_helper.dart';
 import 'theming/feeel_themes.dart';
-import 'package:window_size/window_size.dart';
+// import 'package:window_size/window_size.dart';
 
 void main() {
   // debugRepaintRainbowEnabled = true; //todo debug
@@ -46,11 +46,11 @@ class Feeel extends StatelessWidget {
   Widget build(BuildContext context) {
     WidgetsFlutterBinding.ensureInitialized();
 
-    if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
-      setWindowTitle('Feeel');
-      setWindowMinSize(const Size(320, 568));
-      setWindowMaxSize(Size.infinite);
-    }
+    // if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) { //todo not supported on Linux for ARM yet
+    //   setWindowTitle('Feeel');
+    //   setWindowMinSize(const Size(320, 568));
+    //   setWindowMaxSize(Size.infinite);
+    // }
 
     WidgetsBinding.instance
         .addPostFrameCallback((_) => _onWidgetBuilt(context));
@@ -58,22 +58,29 @@ class Feeel extends StatelessWidget {
     SystemChrome.setSystemUIOverlayStyle(
         SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.transparent));
 
-    return AdaptiveTheme(
-        light: FeeelThemes.lightTheme,
-        dark: FeeelThemes.darkTheme,
-        initial: AdaptiveThemeMode.light,
-        builder: (theme, darkTheme) => MaterialApp(
-            title: 'Feeel',
-            theme: theme,
-            darkTheme: darkTheme,
-            supportedLocales: LocaleHelper.supportedLocales,
-            localizationsDelegates: [
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            home: I18n(
-                child: WorkoutListScreen()))); //todo not seeing current locale!
+    return
+        // Provider<FeeelDB>(
+        //     //todo see https://drift.simonbinder.eu/faq/#using-the-database
+        //     create: (context) => FeeelDB(),
+        //     dispose: (context, db) => db.close(),
+        //     child:
+        AdaptiveTheme(
+            light: FeeelThemes.lightTheme,
+            dark: FeeelThemes.darkTheme,
+            initial: AdaptiveThemeMode.light,
+            builder: (theme, darkTheme) => MaterialApp(
+                title: 'Feeel',
+                theme: theme,
+                darkTheme: darkTheme,
+                supportedLocales: LocaleHelper.supportedLocales,
+                localizationsDelegates: [
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                  GlobalCupertinoLocalizations.delegate,
+                ],
+                home: I18n(child: WorkoutListScreen()))
+            //)
+            );
   }
 
   void _onWidgetBuilt(BuildContext context) {
