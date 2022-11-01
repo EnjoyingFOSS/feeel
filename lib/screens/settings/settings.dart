ce1746a5944e8dbd36f6b0160a472654a90dabb6
Miroslav Mazel <contact.mirek@protonmail.com>
2022-11-01 11:15:46 +0100

    Making a string translatable

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -269,8 +269,8 @@ class _SettingsScreenState extends State<SettingsScreen> {
   void _launchURL(String url) async {
     await canLaunchUrl(Uri.parse(url))
         ? await launchUrl(Uri.parse(url))
-        : ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            content: Text("Could not open URL."),
+        : ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text("Could not open URL.".i18n),
           ));
   }
 
