ff04ace63bc77c87bfcfd9974ac9d16871db5d88
Miroslav Mazel <contact.mirek@protonmail.com>
2023-04-16 23:20:14 +0200

    WorkoutRecordSheet

---
+++ lib/screens/activity/components/workout_record_sheet.dart
@@ -0,0 +1,160 @@
+// Copyright (C) 2019 Miroslav Mazel
+//
+// This file is part of Feeel.
+//
+// Feeel is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version. As an additional permission under
+// section 7, you are allowed to distribute the software through an app
+// store, even if that store has restrictive terms and conditions that
+// are incompatible with the AGPL, provided that the source is also
+// available under the AGPL with or without this permission through a
+// channel without those restrictive terms and conditions.
+//
+// Feeel is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Feeel.  If not, see <http://www.gnu.org/licenses/>.
+
+import 'package:feeel/components/exercise_list_item.dart';
+import 'package:feeel/db/database.dart';
+import 'package:feeel/models/full_workout_record.dart';
+import 'package:feeel/providers/db_provider.dart';
+import 'package:feeel/screens/activity/components/workout_record_row.dart';
+import 'package:feeel/theming/feeel_grid.dart';
+import 'package:feeel/theming/feeel_swatch.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class WorkoutRecordSheet extends ConsumerWidget {
+  static const _translucentGrayscaleColorFilter = ColorFilter.matrix([
+    0.2126,
+    0.7152,
+    0.0722,
+    0,
+    0,
+    0.2126,
+    0.7152,
+    0.0722,
+    0,
+    0,
+    0.2126,
+    0.7152,
+    0.0722,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0.25,
+    0,
+  ]);
+
+  final WorkoutRecord workoutRecord;
+  final FeeelSwatch colorSwatch;
+
+  const WorkoutRecordSheet(
+      {super.key, required this.workoutRecord, required this.colorSwatch});
+
+  static void showSheet(BuildContext context, WorkoutRecord workoutRecord,
+      FeeelSwatch colorSwatch) {
+    showModalBottomSheet<void>(
+        context: context,
+        isScrollControlled: true,
+        isDismissible: true,
+        builder: (_) => WorkoutRecordSheet(
+              workoutRecord: workoutRecord,
+              colorSwatch: colorSwatch,
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final db = ref.read(dbProvider);
+    return DraggableScrollableSheet(
+        expand: false,
+        builder: (context, scrollController) {
+          return Align(
+              alignment: Alignment.topCenter,
+              child: ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: LayoutXL.cols8.width),
+                  child: FutureBuilder(
+                      future: db.queryFullWorkoutRecord(
+                          workoutRecord, Localizations.localeOf(context)),
+                      builder: (_, snapshot) {
+                        if (snapshot.hasData) {
+                          final fullWorkoutRecord =
+                              snapshot.data as FullWorkoutRecord;
+                          return CustomScrollView(
+                            shrinkWrap: true,
+                            controller: scrollController,
+                            physics: const ClampingScrollPhysics(),
+                            slivers: [
+                              SliverToBoxAdapter(
+                                  child: Padding(
+                                      padding: const EdgeInsets.fromLTRB(
+                                          0, 8, 0, 16),
+                                      child: WorkoutRecordRow(
+                                          workoutRecord, colorSwatch))),
+                              SliverList(
+                                  delegate: SliverChildBuilderDelegate(
+                                      childCount: fullWorkoutRecord
+                                          .primaryLangFullExercises
+                                          .length, (_, i) {
+                                final fe = fullWorkoutRecord
+                                    .primaryLangFullExercises[i];
+                                final wer =
+                                    fullWorkoutRecord.workoutExerciseRecords[i];
+                                if (wer.completedDuration == 0) {
+                                  return ExerciseListItem(
+                                    primaryLangFullExercise: fe,
+                                    subtitle: Text(
+                                      AppLocalizations.of(context)!
+                                          .txtTimeCompletedOutOfTotal(
+                                              "${wer.completedDuration}",
+                                              AppLocalizations.of(context)!
+                                                  .txtDurationFormatSec(wer
+                                                      .exerciseDuration
+                                                      .toString())),
+                                      style: TextStyle(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .onSurface
+                                              .withAlpha(128)),
+                                    ),
+                                    colorSwatch: colorSwatch,
+                                    colorFilter:
+                                        _translucentGrayscaleColorFilter,
+                                  );
+                                } else {
+                                  return ExerciseListItem(
+                                      primaryLangFullExercise: fe,
+                                      subtitle: Text(
+                                          AppLocalizations.of(context)!
+                                              .txtTimeCompletedOutOfTotal(
+                                                  "${wer.completedDuration}",
+                                                  AppLocalizations.of(context)!
+                                                      .txtDurationFormatSec(wer
+                                                          .exerciseDuration
+                                                          .toString()))),
+                                      colorSwatch: colorSwatch);
+                                }
+                              }))
+                            ],
+                          );
+                        } else {
+                          return Container(
+                              alignment: Alignment.center,
+                              height: 72,
+                              child:
+                                  const CircularProgressIndicator.adaptive());
+                        }
+                      })));
+        });
+  }
+}
